f8498cffab2c174d0ead5852b0fd0323216c8924
Pablo Gonzalez <pabgon0@gmail.com>
2019-07-04 18:05:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,33 +18,9 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-	monopoly = {
-	  :railroads => {
-	    :pieces => 4,
-	    :rent_in_dollars => {
-	      
-	    },
-	    :names => {
-	      
-	    }
-	  }
-	}
+
 end
 
 def monopoly_with_fourth_tier
-	monopoly = {
-	  :railroads => {
-	    :pieces => 4,
-	    :rent_in_dollars => {
-        :one_piece_owned => 25,
-        :two_piece_owned => 50,
-        :three_piece_owned => 100,
-        :four_piece_owned => 200
-	    },
-	    :names => {
-	      
-	    }
-	  }
-	}
 
 end
