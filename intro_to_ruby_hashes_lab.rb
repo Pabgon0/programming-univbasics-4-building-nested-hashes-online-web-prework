b1d51e4470760c0475e38bbb6b8325da30d85385
Pablo Gonzalez <pabgon0@gmail.com>
2019-07-04 18:03:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,8 +1,5 @@
 def monopoly
 	monopoly = {
-	  :railroads => {
-	    
-	  }
 	}
 
 end
@@ -10,13 +7,7 @@ end
 def monopoly_with_second_tier
 	monopoly = {
 	  :railroads => {
-	    :pieces => 4,
-	    :rent_in_dollars => {
-	      
-	    },
-	    :names => {
-	      
-	    }
+	    
 	  }
 	}
 
